5ebb71958e6f9844ebe2435eb430ca989139b4a7
Rishi Raj <rishi.rajsbg@gmail.com>
2024-07-25 06:18:59 +0530

    Reading and serving Html from a file

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const http = require('http');
 const port =8000;
+const fs = require('fs')// module that i require over here
 
 function requestHandler(req, res){
    console.log(req.url);
    res.writeHead(200, {'content-type':'text/html'});
 
-   res.end('<h1>Gotcha!</h1>')
+   fs.readFile('./index.html', function(err,data){  /// readfile- inbuild ayshrononous function
+         if(err){
+            console.log("error", err);
+            return res.end('<h1> Error</h1>')
+         }
+         return res.end(data)
+   });
+
+  
 }
 
 const server = http.createServer(requestHandler);
