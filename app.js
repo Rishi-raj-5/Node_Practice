64b3eba4bc3d1e9c8bfe52917bd6c44880a09293
Rishi Raj <rishi.rajsbg@gmail.com>
2024-07-25 06:53:32 +0530

    Extending to Multiple pages

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
 const http = require('http');
-const port =8000;
-const fs = require('fs')// module that i require over here
+const port = 8000;
+const fs = require('fs');
+
 
 function requestHandler(req, res){
-   console.log(req.url);
-   res.writeHead(200, {'content-type':'text/html'});
+    console.log(req.url);
+    res.writeHead(200, {'content-type': 'text/html'});
+    
+    
+    let filePath;
 
-   fs.readFile('./index.html', function(err,data){  /// readfile- inbuild ayshrononous function
-         if(err){
-            console.log("error", err);
-            return res.end('<h1> Error</h1>')
-         }
-         return res.end(data)
-   });
+    switch(req.url){
+        case '/':
+            filePath = './index.html'
+            break;
+        case '/profile':
+            filePath = './profile.html'
+            break;
+        default:
+            filePath = './404.html'    
+    }
 
-  
+    fs.readFile(filePath, function(err,data){
+        if(err){
+            console.log('error', err);
+            return res.end('<h1>Error!</h1>');
+        }
+        return res.end(data);
+    })
+    
 }
 
 const server = http.createServer(requestHandler);
 
+
+
 server.listen(port, function(err){
-     if(err){
+    if(err){
         console.log(err);
         return;
-     }
-     console.log("Server is running on port", port);
+    }
+
+    console.log("Server is up and running on port:", port);
 });
